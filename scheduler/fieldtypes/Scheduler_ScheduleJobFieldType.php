ffcc6a95dadf21f352acdab93dee82afc09674ec
Josh Angell <josh@supercooldesign.co.uk>
2016-06-08 10:52:27 +0100

    Fieldtype added that stores a date for a given job type, doesn’t yet add the job

---
+++ scheduler/fieldtypes/Scheduler_ScheduleJobFieldType.php
@@ -0,0 +1,143 @@
+<?php
+namespace Craft;
+
+/**
+ * Scheduler by Supercool
+ *
+ * @package   Scheduler
+ * @author    Josh Angell
+ * @copyright Copyright (c) 2016, Supercool Ltd
+ * @link      http://plugins.supercooldesign.co.uk
+ */
+
+class Scheduler_ScheduleJobFieldType extends DateFieldType
+{
+
+	// Public Methods
+	// =========================================================================
+
+	/**
+	 * @inheritDoc IComponentType::getName()
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Craft::t('Schedule Job');
+	}
+
+	/**
+	 * @inheritDoc ISavableComponentType::getSettingsHtml()
+	 *
+	 * @return string|null
+	 */
+	public function getSettingsHtml()
+	{
+		$typeOptions = $this->_getTypeOptions();
+
+		$incrementOptions = array(15, 30, 60);
+		$incrementOptions = array_combine($incrementOptions, $incrementOptions);
+
+		return craft()->templates->render('scheduler/fieldtypes/ScheduleJob/settings', array(
+			'typeOptions' => $typeOptions,
+			'incrementOptions' => $incrementOptions,
+			'settings' => $this->getSettings(),
+		));
+	}
+
+	/**
+	 * @inheritDoc IFieldType::getInputHtml()
+	 *
+	 * @param string $name
+	 * @param mixed  $value
+	 *
+	 * @return string
+	 */
+	public function getInputHtml($name, $value)
+	{
+		$variables = array(
+			'id'              => craft()->templates->formatInputId($name),
+			'name'            => $name,
+			'value'           => $value,
+			'minuteIncrement' => $this->getSettings()->minuteIncrement
+		);
+
+		$input = '<div class="datetimewrapper">';
+		$input .= craft()->templates->render('_includes/forms/date', $variables);
+		$input .= ' '.craft()->templates->render('_includes/forms/time', $variables);
+		$input .= '</div>';
+
+
+		return $input;
+	}
+
+	/**
+	 * @inheritDoc ISavableComponentType::prepSettings()
+	 *
+	 * @param array $settings
+	 *
+	 * @return array
+	 */
+	public function prepSettings($settings)
+	{
+		return $settings;
+	}
+
+	// Private Methods
+	// =========================================================================
+
+	/**
+	 * Prep the Job type options
+	 *
+	 * @return array
+	 */
+	private function _getTypeOptions()
+	{
+		$typeOptions = array(
+			array(
+				'label' => 'Re-save element',
+				'value' => 'Scheduler_ReSaveElementJob',
+				'default' => true
+			),
+			array(
+				'label' => 'Somethign else',
+				'value' => 'Tatatat'
+			)
+		);
+
+		// TODO: HOOK
+		// TODO: add another job for saving parent element if the current one doesn’t pick it up
+
+		return $typeOptions;
+	}
+
+	// Protected Methods
+	// =========================================================================
+
+	/**
+	 * @inheritDoc BaseSavableComponentType::defineSettings()
+	 *
+	 * @return array
+	 */
+	protected function defineSettings()
+	{
+		$options = array();
+		$default = '';
+		$typeOptions = $this->_getTypeOptions();
+
+		foreach ($typeOptions as $typeOption) {
+			$options[] = $typeOption['value'];
+
+			if (isset($typeOption['default']) && $typeOption['default'])
+			{
+				$default = $typeOption['value'];
+			}
+		}
+
+		return array(
+			'type' => array(AttributeType::Enum, 'values' => $options, 'default' => $default),
+			'minuteIncrement' => array(AttributeType::Number, 'default' => 30, 'min' => 1, 'max' => 60),
+		);
+	}
+
+}
