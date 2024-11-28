d7615c56f370e7110c9d318ffb0a1ba0cda69163
Josh Crawford <joshua@sgroup.com.au>
2024-11-28 18:58:16 +1100

    Update translations

--- src/translations/en/scheduler.php
+++ src/translations/en/scheduler.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'Schedule Job' => 'Schedule Job',
-    'No Job exists with the ID “{id}”' => 'No Job exists with the ID “{id}”',
-    'Job Type' => 'Job Type',
-    'Minute Increment' => 'Minute Increment',
+  'An exception was thrown while trying to save the element with the ID “' => 'An exception was thrown while trying to save the element with the ID “',
+  'Job Type' => 'Job Type',
+  'Minute Increment' => 'Minute Increment',
+  'No Job exists with the ID “{id}”' => 'No Job exists with the ID “{id}”',
+  'Schedule Job' => 'Schedule Job',
 ];
